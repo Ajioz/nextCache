ea99b278772f966a94e0c6009f736ecedddbf466
Sunday Ajiroghene <sunny.ajiroghene@gmail.com>
2024-08-07 07:14:15 +0100

    caching with selfreached db

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,4 +1,5 @@
 import Messages from '@/components/messages';
+import { getMessages } from '@/lib/messages';
 import { unstable_noStore } from 'next/cache';
 
 
@@ -6,15 +7,15 @@ import { unstable_noStore } from 'next/cache';
 // export const dynamic = 'force-dynamic'  //works as no-store cache
 
 
-export default async function MessagesPage() {
-  unstable_noStore();   // this is a granular approach to not caching sensitive fast changing content/dynamic
-  const response = await fetch('http://localhost:8080/messages', {
-    next: {
-      tags : ['msg']
-    }
-  });
-  const messages = await response.json();
-
+export default function MessagesPage() {
+  // unstable_noStore();   // this is a granular approach to not caching sensitive fast changing content/dynamic
+  // const response = await fetch('http://localhost:8080/messages', {
+  //   next: {
+  //     tags : ['msg']
+  //   }
+  // });
+  // const messages = await response.json();
+  const messages = getMessages();
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
   }
